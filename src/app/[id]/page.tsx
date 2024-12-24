e8b52d1476ad6c7d433523a90dca58cd89f7e010
Ali-Asif1 <aliasif1485@yahoo.com>
2024-12-24 22:44:54 +0500

    firt commit

---
+++ src/app/[id]/page.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+const getUser = async (id:number) => {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/users/${id}`
+  );
+
+  if (!response.ok) {
+    throw new Error("failed to fetch data");
+  }
+  return response.json();
+};
+
+const UsersDetail =async ({ params:{id} }) => {
+  const userData = await getUser(id)
+  
+  return (
+    <div className="p-8 space-y-2">
+      <h1 className="underline text-2xl font-semibold">User Info</h1>
+      <p>id: {userData.id}</p>
+      <p>Name: {userData.name}</p>
+      <p>User Name:{userData.username}</p>
+      <p>Phone: {userData.phone}</p>
+      <p>Email {userData.email}</p>
+    </div>
+  )
+};
+
+export default UsersDetail
